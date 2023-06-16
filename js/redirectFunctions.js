cdeda0488ee88b7abebb8062696b0d9553057afc
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2023-06-16 21:53:42 +0530

    updates

--- js/redirectFunctions.js
+++ js/redirectFunctions.js
@@ -1,7 +1,17 @@
-import config from '../config/config.prod.json';
-
 function redirectToSignIn() {
   setTimeout(function () {
-    window.location.href = config.redirect.signInPageUrl;
+    window.location.href = 'pages/signInPage.html';
   }, 1000);
+}
+
+function goToSignUp() {
+  window.location.href = 'signUpPage.html';
+}
+
+function goToSignIn() {
+  window.location.href = 'signInPage.html';
+}
+
+function goToIndex() {
+  window.location.href = '../index.html';
 }
